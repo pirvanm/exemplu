9fffac0586935f57a2496e32f0e34b10c4da5fab
Marian <pirvan.marian@gmail.com>
2023-12-11 16:24:28 +0200

    Genertare controller standard folosind flagul resource , vezi php artisan make:controller HistroyOrdeders --resource

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::all();
+        return view('orders.index', compact('orders'));
+    }
+
+    public function create()
+    {
+        return view('orders.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate and store the order
+    }
+
+    public function show($id)
+    {
+        $order = Order::findOrFail($id);
+        return view('orders.show', compact('order'));
+    }
+
+    public function edit($id)
+    {
+        $order = Order::findOrFail($id);
+        return view('orders.edit', compact('order'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate and update the order
+    }
+
+    public function destroy($id)
+    {
+        // Delete the order
+    }
+}
