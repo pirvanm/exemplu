353b5a4bd05f79991688d3eaccb0d788019f0d54
Marian <pirvan.marian@gmail.com>
2023-12-11 16:50:40 +0200

    migrare pentru definirea unei tabele, pentru a stoca coloane si tipurile acestor coloane

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Definire Structura Tabela
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
